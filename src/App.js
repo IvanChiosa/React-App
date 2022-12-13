9208430da5b55922257a9b241d41f3c25ec95d20
Ivan <i85chiosa@gmail.com>
2022-12-13 16:19:13 +0100

    React App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
 
-const App = () =>{
+import GoalList from './components/GoalList/GoalList.js';
+import NewGoal from './components/NewGoal/NewGoal.js';
+import './App.css';
+
+const App = () => {
+  const courseGoals = [
+    { id: 'cg1', text: 'Finish the course Ivan' },
+    { id: 'cg2', text: 'Finish the course you will be more expert in React' },
+    { id: 'cg3', text: 'Finish the course make you more expert ' },
+  ];
+
+  const addNewGoalHandler = (newGoal) => {
+    courseGoals.push(newGoal);
+  }
+
+
   return (
-    <h1 title="This work">Hallo Ivan</h1>  // React.createElement('h1', {}, 'Hallo von meine seite')
+    // <h1 title="This work">Hallo Ivan</h1>  // React.createElement('h1', {}, 'Hallo von meine seite')
+    <div className='course-goals'>
+      
+      <h2>Course Goals</h2>
+      <NewGoal onAddGoal={addNewGoalHandler}/>
+      <GoalList goals={courseGoals}/>
+      
+    </div>
   );
 }
 
