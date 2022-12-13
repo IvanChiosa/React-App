0e761baafa8a8b213cf4aca003a80b3c6b62a882
Ivan <i85chiosa@gmail.com>
2022-12-13 11:39:07 +0100

    React App

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () =>{
+  return (
+    <h1 title="This work">Hallo Ivan</h1>  // React.createElement('h1', {}, 'Hallo von meine seite')
+  );
+}
+
+export default App;
